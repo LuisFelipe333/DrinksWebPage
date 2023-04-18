61b635600b10eb2de5da5271e0b460b43cdf1dc5
LuisFelipe333 <luigifelipe333@gmail.com>
2023-04-18 01:22:34 -0600

    Change location of models and repository folder

--- Backend/drinks/src/main/java/com/bluelight/drinks/models/DrinkType.java
+++ Backend/drinks/src/main/java/com/bluelight/drinks/models/DrinkType.java
@@ -1,4 +1,4 @@
-package models;
+package com.bluelight.drinks.models;
 
 import jakarta.persistence.*;
 
--- Backend/drinks/src/main/java/com/bluelight/drinks/repository/DrinkTypeRepository.java
+++ Backend/drinks/src/main/java/com/bluelight/drinks/repository/DrinkTypeRepository.java
@@ -1,6 +1,6 @@
-package repository;
+package com.bluelight.drinks.repository;
 
-import models.DrinkType;
+import com.bluelight.drinks.models.DrinkType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
